504a2acd0a7d3a52fb80a43ae58bb66caa205c9d
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-12 17:20:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,41 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = ['A','B','C','D']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
+  return array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.shift(element)
+  return array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
+  return array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.unshift()
+  return array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
