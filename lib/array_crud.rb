cc64f370eff4edd0680442d42fab421285869a21
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-12 17:21:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,17 +13,14 @@ end
 
 def add_element_to_start_of_array(array, element)
   array.shift(element)
-  return array
 end
 
 def remove_element_from_end_of_array(array)
   array.pop()
-  return array
 end
 
 def remove_element_from_start_of_array(array)
   array.unshift()
-  return array
 end
 
 def retrieve_element_from_index(array, index_number)
